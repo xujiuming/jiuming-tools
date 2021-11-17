e08965eafc9abfd5ce73b630a69c8495c53fa983
许久明 <xujiuming@mucang.cn>
2021-11-17 18:15:16 +0800

    chore:更新1.98

---
+++ codeTemplate/scapytest.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+# author ming
+# install  PyX its
+# https://stackoverflow.com/questions/50914059/pyx-not-installed-correctly-when-using-scapy
+# sudo apt install texlive-latex-base
+
+from scapy.utils import rdpcap
+
+if __name__ == '__main__':
+    # 读取 $PATH
+    a=rdpcap('./test.cap')
+    print(a[0])
+    a.pdfdump('t.pdf')
+
