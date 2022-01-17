781cabe1ac3ebe115cddd6075a8b99664de10524
许久明 <xujiuming@mucang.cn>
2022-01-17 11:15:56 +0800

    feat:爬取51yip的linux指令

---
+++ codeTemplate/51yipRequest.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+# author ming
+# 调用http://linux.51yip.com 页面 获取对应命令的使用方式文档
+# requests + lxml
+import requests
+from lxml import etree
+
+if __name__ == '__main__':
+    response = requests.get("http://linux.51yip.com/search/ls")
+    response.encoding = 'utf-8'
+    print(response.text)  # 以文本形式打印网页源码
+    # 利用etree.HTML，将字符串解析为HTML文档
+    html = etree.HTML(response.text, etree.HTMLParser())
+    print("---------------------------------------")
+    print(html.xpath('string(//*[@id="post-1"]/h1)'))
+    print(html.xpath('string(//*[@id="post-1"]/div[1])'))
+    content_arr = html.xpath('//*[@id="post-1"]/div[2]/pre')
+    for i, content in enumerate(content_arr):
+        if i == 0:
+            help_arr = content.xpath('text()')
+            for h in help_arr:
+                print(h)
+        else:
+            print(content.xpath('string(.)'))
